b80bd8362d5579c3f79ea5d638a08189e0e92482
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-02-22 15:32:04 +0300

    Summ of digits

---
+++ HomeWork4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputInt(string msg)
+{
+    System.Console.Write($"{msg} >");
+    return (Convert.ToInt32(Console.ReadLine()));
+}
+
+int SumDigits(int num)
+{
+    int res = 0;
+    while (num > 0)
+    {
+        res += num % 10;
+        num /= 10;
+    }
+    return (res);
+}
+
+int number = InputInt("Введите число");
+System.Console.WriteLine($"Сумма цифр числа {number} равна {SumDigits(number)}.");
